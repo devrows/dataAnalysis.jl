c0dec63af169e12ae36195de99154d27486b7791
devrows <drose02@mail.uoguelph.ca>
2015-09-25 13:42:20 -0400

    Added a function to find the potential error intervals

--- src/findError.jl
+++ src/findError.jl
@@ -5,10 +5,10 @@ Functions used to analyze the peak data
 """
 
 
-
+#need to be able to find the indexing for errorIntervals to access the proper indexing
 function calculateBackgroundMinimumError(errorIntervals::Array, parsedArray::Array)
   """
-  This function finds the minimum error that can be used as the background error
+  This function finds the minimum error that can be used as the background error. Returns
   """
   largestInterval = Array(Float64, 1, 3)
   largestInterval[1,:] = 0
@@ -39,3 +39,44 @@ end
 
 
 
+function errorIntervals(parsedArray::Array, mean::Float64)
+"""
+Finds the intervals of values that are below the mean of the entire data
+"""
+  errorIntervals = Array(Float64, 1, 2)
+  errorIntervals[1,:] = 1
+
+  #find bounds error later
+  for i = 2:size(parsedArray)[1]
+    #beginning of interval
+    if parsedArray[i,2] < meanOfAllData && parsedArray[i-1,2] > meanOfAllData
+      #create a new array to add to the end of the array
+      elementToAdd = Array(Float64, 1, 2)
+      elementToAdd[1,1] = parsedArray[i,1]
+      elementToAdd[1,2] = 0
+
+      #finds the end of the interval
+      while (elementToAdd[1,2] == 0 && i < size(parsedArray)[1])
+
+        i+=1
+
+        #end of interval conditions
+        if parsedArray[i,2] > meanOfAllData && parsedArray[i-1,2] < meanOfAllData
+          #vertically concatenate the array
+          elementToAdd[1,2] = parsedArray[i,1]
+          errorIntervals = vcat(errorIntervals, elementToAdd)
+        end
+
+        #last value if the last interval is < meanOfData
+        if elementToAdd[1,2] == 0 && i == size(parsedArray)[1]
+          elementToAdd[1,2] = parsedArray[i,1]
+          errorIntervals = vcat(errorIntervals, elementToAdd)
+        end
+      end #while loop
+    end #ends if conditions for the beginning of the loop
+  end #ends for loop to find all intervals with values less than the mean
+
+#errorIntervals
+
+  return errorIntervals
+end
